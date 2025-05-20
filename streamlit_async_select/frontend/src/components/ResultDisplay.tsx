f467592d172511361e6119823e7c5941b9a981c8
Liam Power <liamfpower@gmail.com>
2025-05-20 12:19:40 +1000

    feat: fix aspect_ratio

--- streamlit_async_select/frontend/src/components/ResultDisplay.tsx
+++ streamlit_async_select/frontend/src/components/ResultDisplay.tsx
@@ -3,18 +3,23 @@ import { AspectRatio } from "./ui/aspect-ratio"
 export function ResultDisplay({ image, title, description }: { image: string; title: string; description: string }) {
     return (
         <div className="flex items-center gap-3">
-            <div className="w-10 h-10">
+            <div className="w-8 h-8">
                 <AspectRatio ratio={1}>
                     <img
                         src={image}
                         alt={title}
-                        className=""
+                        className="w-full h-full"
+                        style={{
+                            objectFit: "cover",
+                            width: "100%",
+                            height: "100%",
+                        }}
                     />
                 </AspectRatio>
             </div>
             <div className="flex flex-col">
-                <div className="font-semibold">{title}</div>
-                <div className="text-xs text-muted-foreground">{description}</div>
+                <div className="font-semibold text-ellipsis overflow-hidden">{title}</div>
+                <div className="text-xs text-muted-foreground text-ellipsis overflow-hidden">{description}</div>
             </div>
         </div>
     );
